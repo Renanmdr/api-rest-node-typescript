90cb947f623b3eca17efedd9d93bc897feeeeb4d
Renanmdr <renanmdr3@gmail.com>
2024-04-05 16:12:16 -0300

    feat: cors configured

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
 import './shared/services/translationsYup';
+import cors from 'cors';
 import { router } from './routes';
 
 const server = express();
+
+server.use(cors({
+  origin: process.env.ENABLED_CORS?.split(';') || []
+}));
 
 server.use(express.json());
 
